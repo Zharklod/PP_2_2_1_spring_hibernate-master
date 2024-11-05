22c22692e810a1b82f1e1149db9732931d9c5e27
Zharklod <d.zharckov2013@yandex.ru>
2024-11-05 14:18:58 +0300

    Initial commit

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,42 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("BMW", "X5")));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("AUDI", "A4")));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Wolkswagen", "Passat")));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("УАЗ", "Патриот")));
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
+         System.out.println("Car model = " + user.getUserCar().getModel());
+         System.out.println("Car series = " + user.getUserCar().getSeries());
+         System.out.println();
+      }
+
+      System.out.println(userService.getUserByCarDetails("BMW", "X5").toString());
+      System.out.println(userService.getUserByCarDetails("AUDI", "A4").toString());
+      System.out.println(userService.getUserByCarDetails("Wolkswagen", "Passat").toString());
+      System.out.println(userService.getUserByCarDetails("УАЗ", "Патриот").toString());
+
+      context.close();
+   }
+}
